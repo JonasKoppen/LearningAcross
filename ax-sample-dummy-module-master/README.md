cf32fa203e8613723f37c775e6cd869812838737
jonas <dreaddrone@gmail.com>
2020-07-15 21:03:16 +0200

    Adding fixed dummy AX module

---
+++ ax-sample-dummy-module-master/README.md
@@ -0,0 +1,4 @@
+# Across Module Samples: Dummy module
+
+The dummy module is one of the samples used in the Starter trails.
+This module logs a simple message when the module is bootstrapped and serves no other purpose than to be used as an example.
---
+++ ax-sample-dummy-module-master/src/main/java/com/foreach/across/samples/modules/dummy/config/DummyModuleConfiguration.java
@@ -0,0 +1,22 @@
+package com.foreach.across.samples.modules.dummy.config;
+
+import com.foreach.across.samples.modules.dummy.DummyModule;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author Steven Gentens
+ * @since 0.0.1
+ */
+@Configuration
+@Slf4j
+public class DummyModuleConfiguration {
+
+    @PostConstruct
+    public void isBootstrapped() {
+        LOG.info( DummyModule.NAME + " has been added to the application." );
+    }
+
+}
